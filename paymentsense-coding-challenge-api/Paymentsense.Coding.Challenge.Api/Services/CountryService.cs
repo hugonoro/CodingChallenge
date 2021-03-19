1ca7f056479ccb432e099fc8f9e2d110e6f6831b
Hugo Noro <norogithub@gmail.com>
2021-03-19 20:07:15 +0000

    [API] - Setup HTTP client to connect to countries API

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Paymentsense.Coding.Challenge.Api.Models;
 
 namespace Paymentsense.Coding.Challenge.Api.Services
 {
     public class CountryService: ICountryService
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+
+        public CountryService(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
         public async Task<Country[]> GetAll()
         {
-            throw new NotImplementedException();
+            var countriesApi = _configuration.GetValue<string>("CountriesAPI");
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.GetAsync($"{countriesApi}/all");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var countryList = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Country[]>(countryList);
+            }
+
+            throw new Exception("An error occurred while connecting to the countries API endpoint.");
         }
     }
 }
