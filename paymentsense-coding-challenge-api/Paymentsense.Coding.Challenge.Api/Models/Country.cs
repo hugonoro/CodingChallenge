b29dda78e47a94f6c1c5c8765d9a58cc5267f1be
Hugo Noro <norogithub@gmail.com>
2021-03-21 19:51:06 +0000

    [API] - Task 4 - Added additional fields to country list payload

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Models/Country.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Models/Country.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Paymentsense.Coding.Challenge.Api.Models
 {
     public class Country
     {
         public string Name { get; set; }
         public string Flag { get; set; }
+        public int Population { get; set; }
+        public string Capital { get; set; }
+        public IEnumerable<string> TimeZones { get; set; }
+        public IEnumerable<Currency> Currencies { get; set; }
+        public IEnumerable<Language> Languages { get; set; }
+    }
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Language
+    {
+        public string Iso639_1 { get; set; }
+        public string Iso639_2 { get; set; }
+        public string name { get; set; }
+        public string NativeName { get; set; }
     }
 }
